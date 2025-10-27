bf89fd5331a488ef2ed478ef966a41c949aac172
Alisson Silva <alisson.bwt@outlook.com>
2025-10-27 19:31:40 -0300

    Remove test data via API

--- frontend/cypress/e2e/features/client.cy.ts
+++ frontend/cypress/e2e/features/client.cy.ts
@@ -1,19 +1,18 @@
 describe('Client', () => {
+    let documentId:string = '54894415003';
+
     beforeEach(() => {
         cy.visit('/')
     })
 
     it('Should create a new client', () => {
-        // login
         cy.login('admin', '123');
-        // go to client page
         cy.visit('/clients');
 
-        // click on "create" button
         cy.clickByTestId('create-client-button');
-        // fill in form data
+
         cy.typeByTestId('newclient-fullname-field', 'Fábio Iago Corte Real');
-        cy.typeByTestId('newclient-document-field', '47584368786');
+        cy.typeByTestId('newclient-document-field', '54894415003');
         cy.typeByTestId('newclient-phone-field', '(61) 98471-5344');
         cy.typeByTestId('newclient-email-field', 'fabioiagocortereal@imagemeaudio.com.br');
         cy.typeByTestId('newclient-zipcode-field', '72327-011');
@@ -25,9 +24,26 @@ describe('Client', () => {
         cy.typeByTestId('newclient-streetcomplement-field', 'Quadra QR 425 Conjunto 11');
         cy.clickByTestId('newclient-create-button');
 
-        // verify created client in list
         cy.get('[data-testid="client-page-table"] > tbody > tr').eq(0).within(() => {
             cy.get('td').eq(0).should('have.text', 'Fábio Iago Corte Real');
+        })
+    })
+
+    //Remove data
+    after(() => {
+        cy.request({
+            method: 'GET',
+            url: `http://localhost:5196/api/Client/documentId/${documentId}`
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            let clientId = response.body.id;
+
+            cy.request({
+                method: 'DELETE',
+                url: `http://localhost:5196/api/Client/${clientId}`
+            }).then((response) => {
+                expect(response.status).to.eq(204);
+            })
         })
     })
 })
